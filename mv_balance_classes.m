24d45e122db09a07ab6570828c5b68bbe11b0e96
Matthias Treder <matthias.treder@gmail.com>
2017-06-24 19:16:37 +0100

    allows for selecting a specific number of trials now

--- mv_balance_classes.m
+++ mv_balance_classes.m
@@ -1,4 +1,4 @@
-function [X,labels] = mv_balance_classes(X,labels,method,replace)
+function [X,labels] = mv_balance_classes(X,labels,method,replace,num)
 %Balances data with imbalanced classes by oversampling the minority class
 %or undersampling the majority class.
 %
@@ -10,7 +10,10 @@ function [X,labels] = mv_balance_classes(X,labels,method,replace)
 %                  or [number of samples x number of features] data matrix.
 % labels         - [1 x number of samples] vector of class labels containing
 %                  1's (class 1) and -1's (class 2)
-% method         - 'oversample' or 'undersample'
+% method         - 'oversample' or 'undersample'. Alternatively, an integer
+%                  number can be given which will be the number of samples
+%                  in each class. Undersampling or oversampling of each
+%                  class will be used to create it.
 % replace        - for oversampling, if 1, data is oversampled with replacement (like in
 %                  bootstrapping) i.e. samples from the minority class can
 %                  be added 3 or more times. If 0, data is oversampled
@@ -43,35 +46,73 @@ if nargin<4 || isempty(replace)
     replace = 1;
 end
 
-N1 = sum(labels==1);
-N2 = sum(labels== -1);
+N = [sum(labels==1), sum(labels== -1)];
+% N1 = sum(labels==1);
+% N2 = sum(labels== -1);
 
 %% Determine which class is the minority class
-if N1 < N2
+if N(1) < N(2)
     minorityClass = 1;
 else
     minorityClass = -1;
 end
 
 %% Oversample/undersample
-addRmSamples = abs(N1-N2);  % number of samples to be added/removed
+addRmSamples = abs(N(1)-N(2));  % number of samples to be added/removed for over/undersampling
 
-if strcmp(method,'oversample')
+if ischar(method) && strcmp(method,'oversample')
     % oversample the minority class
     idxMinority = find(labels == minorityClass);
     if replace
-        idxAdd = randi( min(N1,N2), addRmSamples, 1);
+        idxAdd = randi( min(N(1),N(2)), addRmSamples, 1);
     else
-        idxAdd = randperm( min(N1,N2), addRmSamples);
+        idxAdd = randperm( min(N(1),N(2)), addRmSamples);
     end
     X= cat(1,X, X(idxMinority(idxAdd),:,:));
     labels(end+1:end+addRmSamples)= labels(idxMinority(idxAdd));
     
-elseif strcmp(method,'undersample')
+elseif ischar(method) && strcmp(method,'undersample')
     % undersample the majority class
     idxMajority = find(labels == -1*minorityClass);
-    idxRm = randperm( max(N1,N2), addRmSamples);
+    idxRm = randperm( max(N(1),N(2)), addRmSamples);
     X(idxMajority(idxRm),:,:)= [];
     labels(idxMajority(idxRm))= [];
+   
+elseif isnumeric(method)
+    % The target number of samples is directly provided as a number. Each
+    % class will be either over- or undersampled to provide the target
+    % number of trials. For instance, if N = [10 20] and num = 5, then both
+    % classes will be undersampled such that there is 5 samples in each
+    % class. If num=15, then the first class will be oversampled (adding 5
+    % samples) and the second class will be undersampled (removing 5
+    % samples). If num=30, both classes will be oversampled.
+    num = method;
+    
+    % First check whether we need to over- or undersample each class
+    do_undersample = [N(1) N(2)] > num;
+
+    for cc=1:2
+        y = (-1)^(cc+1);  % -1 for ii=1 and 1 for cc=2
+        idxClass= find(labels == y); % class indices for class cc
+        
+        if do_undersample(cc)
+            % We need to undersample this class
+            idxRm = randperm( N(cc), N(cc)-num );
+            X(idxClass(idxRm),:,:)= [];
+            labels(idxClass(idxRm))= [];
+            
+        else
+            % We need to oversample this class
+            if num-N(cc)>0
+                if replace
+                    idxAdd = randi( N(cc), num-N(cc), 1);
+                else
+                    idxAdd = randperm( N(cc), num-N(cc));
+                end
+                X= cat(1,X, X(idxClass(idxAdd),:,:));
+                labels(end+1:end+num-N(cc))= labels(idxClass(idxAdd));
+            end
+        end
+    end
 end
 
