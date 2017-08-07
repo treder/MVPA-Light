043418a4ebec15657a9f8adc781ac11c8c26bf2b
Matthias Treder <matthias.treder@gmail.com>
2017-08-07 23:54:33 +0100

    renamed functions fetching classifier output/performance

--- utils/mv_classifier_output.m
+++ utils/mv_classifier_output.m
@@ -1,14 +1,13 @@
-function out = mv_get_classifier_output(output, cf, test_fun, Xtest)
+function out = mv_classifier_output(output, cf, test_fun, Xtest)
 %Convenience function that fetches classifier output, i.e. either the 
-%predicted class labels or decision values. Used in
-%mv_classify_across_time and mv_classify_timextime.
+%predicted class labels or decision values.
 %
 %Usage:
 % out = mv_get_classifier_output(output, cf, test_fun, Xtest)
 %
 %Parameters:
-% output            - output type, 'label' (predicted class labels) or 
-%                     'dval' (decision values)
+% output            - output type, 'label' (predicted labels) or 'dval'
+%                     (decision values)
 % cf                - classifier, output of a train_* function
 % test_fun          - classifier test function, a test_* function
 % Xtest             - [nSamples x nFeatures] test data
@@ -23,7 +22,8 @@ switch(output)
                
     case 'dval'
         % Note that the test function should be able to return decision
-        % values as second argument. Not all classifiers provide decision
-        % values (eg Random Forests)
-        [~, out] = test_fun(cf,Xtest);       
+        % values as second argument. This does not work with all
+        % classifiers (eg Random Forests)
+        [~, out] = test_fun(cf,Xtest);
+        
 end
