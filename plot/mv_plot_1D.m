beabf111f414badd8a42ee0984d64660b0dbf871
Matthias Treder <matthias.treder@gmail.com>
2017-08-21 20:55:28 +0100

    add boundedopt

--- plot/mv_plot_1D.m
+++ plot/mv_plot_1D.m
@@ -1,23 +1,32 @@
-%% TODO 
-function h = mv_plot_1D(cfg, dat)
+function h = mv_plot_1D(cfg, time, dat, err)
 %Plots 1D results, e.g. classification across time. If a 2D matrix is given 
 %several plots are created.
 %
 %Usage:
-% ax = mv_plot_2D(cfg,M)
+% ax = mv_plot_1D(cfg, time, dat, err)
 %
 %Parameters:
-% DAT               - [N x M] data matrix with results. Plots M lines of
+% time              - [N x 1] vector of times representing the x-axis
+% dat               - [N x M] data matrix with results. Plots M lines of
 %                     length M
+% err               - [N x M] data matrix specifying errorbars (optional) 
+%                     The external boundedline function is used to plot the
+%                     error as a shaded area
 %
-% cfg          - struct with hyperparameters:
-% xlabel,ylabel     - label for x and y axes (default '')
+% cfg          - struct with hyperparameters (use [] to keep all parameters at default):
+% xlabel,ylabel     - label for x and y axes (defaults 'Time' and 'Accuracy')
 % title             - axis title (default '')
-% x                 - x values (e.g. time points)
 % grid              - options for the grid function (default {'on'})
-% zero              - marks the zero point with a horizontal and vertical
-%                     line. Give the line options as cell array (default 
+% lineorder         - order of line types when multiple lines are plotted
+%                     (default {'-' '--' ':'})
+% hor               - y-value corresponding to horizontal line (default 0.5)
+% ver               - x-value corresponding to vertical line (default 0)
+% cross             - Give the line options for horizontal and vertical
+%                     lines forming a crosshair as cell array (default 
 %                     {'--k'}). Set to [] to remove lines
+% bounded           - cell array with additional arguments passed to
+%                     boundedline.m when a plot with errorbars is created
+%                     (default {'alpha'})
 %
 % Returns:
 % h        - struct with handles to the graphical elements 
@@ -26,105 +35,60 @@ function h = mv_plot_1D(cfg, dat)
 
 [nX,nY] = size(dat);
 
-mv_setDefault(cfg,'x',1:nX);
-mv_setDefault(cfg,'xlabel','');
-mv_setDefault(cfg,'ylabel','');
+if nargin<3, err=[]; end
+
+mv_setDefault(cfg,'xlabel','Time');
+mv_setDefault(cfg,'ylabel','Accuracy');
 mv_setDefault(cfg,'title','');
 mv_setDefault(cfg,'grid',{'on'});
-mv_setDefault(cfg,'zero',{'--k'});
-
-
-if ~iscell(cfg.grid), cfg.grid={cfg.grid}; end
-if ~iscell(cfg.xlabel)
-    cfg.xlabel = repmat({cfg.xlabel},[1 P]);
-end
-if ~iscell(cfg.ylabel)
-    cfg.ylabel = repmat({cfg.ylabel},[1 P]);
-end
-if ~iscell(cfg.title)
-    cfg.title = repmat({cfg.title},[1 P]);
-end
+mv_setDefault(cfg,'lineorder',{'-' '-' '--' '--' ':'});
+mv_setDefault(cfg,'hor',0.5);
+mv_setDefault(cfg,'ver',0);
+mv_setDefault(cfg,'cross',{'--k'});
+mv_setDefault(cfg,'bounded',{'alpha'});
 
 h = struct();
 h.ax = gca;
+cla
 
-x= cfg.x;
-
-%% Plot data matrix
-axnum=1;
-for rr=1:cfg.nrow
-    for cc=1:cfg.ncol
-        if axnum<=P
-            h.ax(axnum) = subplot(cfg.nrow,cfg.ncol,axnum);
-            set(gcf,'CurrentAxes',h.ax(axnum));
-            % Paint the image
-            imagesc(cfg.x, cfg.y, squeeze(dat(:,:,axnum)));
-            axnum=axnum+1;
-        end
-    end
+%% Plot data 
+if nargin==4
+    % We use boundedline to plot the error as well
+    tmp = zeros( size(err,1), 1, size(err,2));
+    tmp(:,1,:) = err;
+    h.plt = boundedline(time, dat, tmp, cfg.bounded{:});
+else
+    % Ordinary plot without error
+    h.plt = plot(time, dat);
+end
+%% Set line styles
+for ii=1:nY
+    set(h.plt(ii),'LineStyle',cfg.lineorder{ mod(ii-1,numel(cfg.lineorder)) + 1 })
 end
 
 %% Mark zero line
-if ~isempty(cfg.zero)
-    if x(1)<0 && x(end)>0
-        for ii=1:P
-            set(gcf,'CurrentAxes',h.ax(ii));
-            hold on
-            yl = ylim(gca);
-            plot(gca,[0 0], yl(:), cfg.zero{:})
-            set(gca,'YLim',yl)
-        end
+if ~isempty(cfg.cross)
+    if time(1)<0 && time(end)>0
+        hold on
+        yl = ylim(gca);
+        plot(gca,[1 1] * cfg.ver, yl(:), cfg.cross{:})
+        set(gca,'YLim',yl)
     end
-    if y(1)<0 && y(end)>0
-        for ii=1:P
-            set(gcf,'CurrentAxes',h.ax(ii));
-            hold on
-            xl = xlim(gca);
-            plot(gca,xl(:), [0 0], cfg.zero{:})
-            set(gca,'YLim',yl)
-        end
-    end
-end
-
-%% Add colorbar
-if cfg.colorbar
-    if cfg.globalclim && ~isempty(strfind(lower(cfg.cblocation),'outside'))
-        % We place a single colorbar left/above the first image if
-        % location='WestOutside'/'NorthOutside', right/below of the last 
-        % image if location='EastOutside'/'Southoutside'
-        if any(strcmpi(cfg.cblocation,{'westoutside','northoutside'}))
-            cbpos = 1;
-        else
-            cbpos = P;
-        end
-        
-        oldpos = get(h.ax(cbpos),'Position');
-        cb = colorbar('peer',h.ax(cbpos),'location',cfg.cblocation);
-        h.colorbar = cb;
-        set(h.ax(cbpos),'Position',oldpos);
-    else
-        for ii=1:P
-            h.colorbar(ii) = colorbar('peer',h.ax(ii),'location',cfg.cblocation);
-        end
-    end
+    hold on
+    xl = xlim(gca);
+    plot(gca,xl(:), [1 1] * cfg.hor, cfg.cross{:})
+%     set(gca,'YLim',yl)
 end
 
 %% Add labels and title
-for ii=1:P  
-    if ~isempty(cfg.xlabel{ii}), h.xlabel(ii) = xlabel(h.ax(ii),cfg.xlabel{ii}); end
-    if ~isempty(cfg.ylabel{ii}), h.ylabel(ii) = ylabel(h.ax(ii),cfg.ylabel{ii}); end
-    if ~isempty(cfg.title{ii}), h.title(ii) = title(h.ax(ii),cfg.title{ii}); end
-end
-
-%% Set Y-Dir
-set(h.ax,'YDir',cfg.ydir);
+xlabel(cfg.xlabel);
+ylabel(cfg.ylabel);
+title(cfg.title);
 
 %% Add grid
-for ii=1:P
-    grid(h.ax(ii), cfg.grid{:})
-end
+grid(h.ax, cfg.grid{:})
 
-%% Set ticks and scale
-set(h.ax, 'YLim',[cfg.y(1) cfg.y(end)]);
-set(h.ax, 'XScale',cfg.xscale,'YScale',cfg.yscale);
+%% Set xlim
+xlim([time(1) time(end)])
+
 
