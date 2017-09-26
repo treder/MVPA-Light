672367f5eba914a435f05b1b232520f3697bdc9e
Matthias Treder <matthias.treder@gmail.com>
2017-09-26 19:31:56 +0100

    added

---
+++ utils/startup_MVPA_Light.m
@@ -0,0 +1,11 @@
+function startup_MVPA_Light
+% Adds MVPA-Light and its subfolders to the MATLAB path
+
+MVPA_path = fileparts(fileparts(mfilename('fullpath')));
+
+addpath(MVPA_path);
+addpath(fullfile(MVPA_path,'classifier'));
+addpath(fullfile(MVPA_path,'classifier','optimisation'));
+addpath(fullfile(MVPA_path,'external'));
+addpath(fullfile(MVPA_path,'plot'));
+addpath(fullfile(MVPA_path,'utils'));
