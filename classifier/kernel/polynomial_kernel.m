d6336c0a7c5ce9665ad5d6f8549365b4a39ff895
Matthias Treder <matthias.treder@gmail.com>
2017-10-31 15:23:52 +0000

    added

---
+++ classifier/kernel/polynomial_kernel.m
@@ -0,0 +1,25 @@
+function out = polynomial_kernel(param, x, y)
+% Polynomial kernel given by ker(x,y) = (x' * y + c)^d, where x and y are 
+% the input vectors and c and d are hyperparameters.
+% 
+% Usage:
+% ker = polynomial_kernel(param, X)
+% val = polynomial_kernel(param, x,y)
+% 
+%Parameters:
+% param          - struct with kernel hyperparameters
+% X              - [samples x features] data matrix 
+%             - OR -
+% x,y            - two feature vectors or matrices of feature vectors
+%
+%Output:
+% out            - [samples x samples] kernel matrix or, if x and y are
+%                  provided, the kernel evaluated for x and y (ker(x,y))
+
+if nargin == 2
+    % compute full kernel matrix
+    out = (param.gamma * (x * x') + param.coef0).^param.degree;
+else
+    % evaluate kernel for x and y
+    out = (param.gamma * (x * y') + param.coef0).^param.degree;
+end
