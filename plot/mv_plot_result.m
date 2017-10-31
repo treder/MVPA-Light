99907e2037e5b3989ce55e8fe546fec3df0b2c46
Matthias Treder <matthias.treder@gmail.com>
2017-10-31 15:29:57 +0000

    added

---
+++ plot/mv_plot_result.m
@@ -0,0 +1,318 @@
+function h = mv_plot_result(result, varargin)
+%Provides a simple visual representation of the results obtained with the
+%functions mv_crossvalidate, mv_classify_across_time, mv_classify_timextime, 
+%and mv_searchlight. 
+%
+%The type of plot depends on which of these functions was used. 
+%
+%Usage:
+% h = mv_plot_result(result,<...>)
+%
+%Parameters:
+% result            - results struct obtained from one of the
+%                     classification functions above. A cell array of
+%                     results can be provided (e.g. results for different
+%                     subjects); in this case, all results need to be 
+%                     created with the same function using the same metric.
+%                     
+%Additional arguments can be provided depending on which classification
+%function was used to create the results:
+%
+% MV_CROSSVALIDATE:
+% Usage: h = mv_plot_result(result)
+%
+% Plots the classification result as a barplot. Plots multiple bars and the
+% mean, if multiple result are provided. 
+%
+% MV_CLASSIFY_ACROSS_TIME:
+% Usage: h = mv_plot_result(result,x)
+%
+% Plots the classification result as a line plot. Plots multiple lines and 
+% a mean, if multiple result are provided. 
+% Optionally, a second input x can be provided that specifies the values 
+% for the x axis (e.g. the times in sec).
+%
+% MV_CLASSIFY_TIMExTIME:
+% h = mv_plot_result(result,x,y)
+%
+% Plots the classification result as an image. Plots multiple images and a
+% mean, if multiple result are provided.
+% Optionally, second and third inputs x and y can be provided that 
+% specify the values for the x and y axes (e.g. the times in sec).
+%
+% MV_SEARCHLIGHT:
+% h = mv_plot_result(result,chanlocs)
+%
+% Returns:
+% h        - struct with handles to the graphical elements 
+
+% (c) Matthias Treder 2017
+
+if ~iscell(result), result = {result}; end
+
+nResults = numel(result);
+metric = result{1}.metric;
+fun = result{1}.function;
+
+if numel(unique(cellfun( @(res) res.function, result,'Un',0))) > 1
+    error('All results must come from the same function')
+end
+if numel(unique(cellfun( @(res) res.metric, result,'Un',0))) > 1
+    error('All results must use the same metric')
+end
+
+fprintf('Plotting the results of %s.\n', fun);
+
+%% Extract all performance measures into a matrix
+perf = cellfun( @(res) res.perf, result, 'Un', 0);
+perf_std = cellfun( @(res) res.perf_std, result, 'Un', 0);
+
+if strcmp(fun,'mv_classify_timextime')
+    cat_dim = 3;
+else
+    cat_dim = 2;
+end
+
+if strcmp(metric,'dval')
+    cat_dim = cat_dim + 1;
+end
+
+perf = cat(cat_dim, perf{:});
+perf_std = cat(cat_dim, perf_std{:});
+
+%% Get axis or legend labels
+lab = arrayfun( @(x) [num2str(x) ' (' result{x}.classifier ')'], 1:nResults,'Un',0);
+
+%% If multiple results are given, calculate mean
+if nResults > 1
+    perf_mean = mean(perf,ndims(perf));
+    perf_std_mean = mean(perf_std,ndims(perf_std));
+    mean_lab = 'MEAN';
+end
+
+%% Struct with handles to graphical objects
+h =struct();
+h.ax = [];
+h.title = [];
+
+%% Plot
+switch(fun)
+    
+    
+    %% --------------- MV_CROSSVALIDATE ---------------
+    case 'mv_crossvalidate'
+
+        figure
+        h.ax = gca;
+        if nResults == 1
+            h.bar = bar(perf');
+        else
+            h.bar = bar(1:nResults+1, [perf, perf_mean]');
+            set(gca,'XTick',1:nResults+1, 'XTickLabel',[lab mean_lab])
+        end
+        
+        % Indicate SEM if the bars are not grouped
+        if any(strcmp(metric,{'auc' 'acc'}))
+            hold on
+            errorbar(1:nResults+1,[perf, perf_mean]', [perf_std, perf_std_mean]','.')
+        end
+        
+        % X and Y labels
+        h.ylabel = ylabel(metric);
+        h.fig = gcf;
+        h.title = title(fun,'Interpreter','none');
+        
+        % Set Y label
+        for ii=1:numel(h.ax)
+            h.ylabel(ii) = ylabel(h.ax(ii),metric);
+        end
+
+    %% --------------- MV_CLASSIFY_ACROSS_TIME ---------------
+    case 'mv_classify_across_time'
+        
+        if nargin > 1,  x = varargin{1};
+        else,           x = 1:length(result{1}.perf);
+        end
+        
+        figure
+        cfg = [];
+        if any(strcmp(metric,{'auc', 'acc'}))
+            cfg.hor = 0.5;
+        elseif any(strcmp(metric,{'dval', 'tval'}))
+            cfg.hor = 0;
+        end
+            
+        if strcmp(metric,'dval')
+            % dval: create separate subplot for each result
+            N = size(perf,3);
+            nc = ceil(sqrt(N));
+            nr = ceil(N/nc);
+            h.plt = [];
+            for ii=1:N
+                subplot(nr,nc,ii)
+                tmp = mv_plot_2D(cfg,x, squeeze(perf(:,:,ii)), squeeze(perf_std(:,:,ii)) );
+                legend(lab(ii))
+                h.ax = [h.ax; tmp.ax];
+                h.plt = [h.plt; tmp.plt];
+                h.fig = gcf;
+                h.title = [h.title; title(fun,'Interpreter','none')];
+            end
+        else
+            tmp = mv_plot_1D(cfg,x, perf, perf_std);
+            legend(lab)
+            h.ax = tmp.ax;
+            h.plt = tmp.plt;
+            h.fig = gcf;
+            h.title = title(fun,'Interpreter','none');
+        end
+        
+        % Plot mean
+        if nResults > 1
+            figure
+            tmp = mv_plot_1D(cfg,x, perf_mean, perf_std_mean );
+            set(tmp.plt, 'LineWidth',2);
+            h.ax = [h.ax; tmp.ax];
+            h.plt = [h.plt; tmp.plt];
+            legend({'MEAN'})
+            h.fig(2) = gcf;
+            h.title = [h.title; title([fun ' (MEAN)'],'Interpreter','none')];
+        end
+
+        % Set Y label
+        for ii=1:numel(h.ax)
+            h.ylabel(ii) = ylabel(h.ax(ii),metric);
+        end
+
+    %% --------------- MV_CLASSIFY_TIMEXTIME ---------------
+    case 'mv_classify_timextime'
+
+        if nargin > 1,  x = varargin{1};
+        else,           x = 1:size(result{1}.perf,1);
+        end
+        if nargin > 2,  y = varargin{2};
+        else,           y = 1:size(result{1}.perf,2);
+        end
+        
+        % settings for 2d plot
+        cfg= [];
+        cfg.x   = x;
+        cfg.y   = y;
+        if any(strcmp(metric,{'auc', 'acc'}))
+            cfg.climzero = 0.5;
+        elseif any(strcmp(metric,{'dval', 'tval'}))
+            cfg.climzero = 0;
+        end
+        
+        if strcmp(metric,'dval')
+            % dval: create figure each class
+            hs = cell(1,2);
+            for cl=1:2
+                figure
+                cfg.title = strcat(fun, ' - ' ,lab, ' (class ', num2str(cl),')');
+                hs{cl} = mv_plot_2D(cfg, squeeze(perf(:,cl,:,:)) );
+            end
+            h = [hs{:}];
+            
+            % Plot mean
+            if nResults > 1
+                figure
+                cfg.title = strcat(fun, '-' ,mean_lab);
+                h(numel(h)+1) = mv_plot_2D(cfg, cat(3,squeeze(perf_mean(:,1,:)), squeeze(perf_mean(:,2,:))) );
+            end
+        else
+            cfg.title = strcat(fun, '-' ,lab);
+            h = mv_plot_2D(cfg, perf);
+            
+            % Plot mean
+            if nResults > 1
+                figure
+                cfg.title = strcat(fun, '-' ,mean_lab);
+                h(numel(h)+1) = mv_plot_2D(cfg, perf_mean );
+            end
+        end
+        
+        % set metric as title for colorbar
+        for ii=1:numel(h)
+            set(get(h(ii).colorbar,'title'),'String',metric)
+        end
+        
+
+
+    %% --------------- MV_SEARCHLIGHT ---------------
+    case 'mv_searchlight'
+       
+        if nargin>1
+            % If a struct with channel information is given, we use it to plot
+            % a topography
+            chans = varargin{1};
+            cfg = [];
+            cfg.cbtitle = metric;
+            cfg.clim = 'sym';
+            
+            if any(strcmp(metric,{'auc', 'acc'}))
+                cfg.climzero = 0.5;
+            elseif any(strcmp(metric,{'dval', 'tval'}))
+                cfg.climzero = 0;
+            end
+
+            if isfield(chans,'outline'), cfg.outline = chans.outline; end
+            
+            if strcmp(metric,'dval')
+                % dval: create figure each class
+                hs = cell(1,2);
+                for cl=1:2
+                    figure
+                    cfg.title = strcat(fun, ' - ' ,lab, ' (class ', num2str(cl),')');
+                    hs{cl} = mv_plot_topography(cfg, squeeze(perf(:,cl,:)), chans.pos);
+                    axis on
+                    axis off
+                end
+                h = [hs{:}];
+            else
+                % no dval: all plots in one figure
+                cfg.title = strcat(fun, ' - ' ,lab);
+                h = mv_plot_topography(cfg, perf, chans.pos);
+            end
+            
+            % Plot mean
+            if nResults > 1
+                figure
+                cfg.title = strcat(fun, '-' ,mean_lab);
+                h(numel(h)+1) = mv_plot_topography(cfg, perf_mean, chans.pos);
+            end
+
+        else
+            % If no chans are provided: plot classification performance 
+            % for each feature as a grouped bar graph
+            
+            if strcmp(metric,'dval')
+                % dval: create figure each class
+                hs = cell(1,2);
+                for cl=1:2
+                    figure
+                    hs{cl}.bar = bar(squeeze(perf(:,cl,:))');
+                    hs{cl}.title= title(sprintf('%s - class %d',fun,cl),'Interpreter','none');
+                    hs{cl}.ylabel = ylabel(metric);
+                    set(gca,'XTick',1:nResults,'XTickLabel',lab)
+                end
+                h = [hs{:}];
+            else
+                figure
+                h.bar = bar(perf');
+                h.ylabel = ylabel(metric);
+                set(gca,'XTick',1:nResults,'XTickLabel',lab)
+                h.title= title(fun,'Interpreter','none');
+            end
+            
+            % Plot mean
+            if nResults > 1
+                figure
+                h(2).bar = bar(perf_mean');
+                h(2).ylabel = ylabel(metric);
+                h(2).title= title(strcat(fun, '-' ,mean_lab),'Interpreter','none');
+            end
+        end
+        
+end
+
+grid on
---
+++ plot/mv_plot_topography.m
@@ -0,0 +1,202 @@
+function h = mv_plot_topography(cfg, topo, pos)
+%Plots a topography 
+%
+%Usage:
+% ax = mv_plot_topography(cfg, topo, pos)
+%
+%Parameters:
+% topo              - [C x 1] vector with specifying a topography, where C
+%                     is the number of channels. 
+%                     Alternatively, if a matrix [C x M] is given,
+%                     a subplot with M different topographies is created
+% pos               - [C x 2] matrix of x-y channel positions. 
+%
+% cfg          - struct with parameters:
+% .outline          - cell array specifying the head/nose/ears cfg.outline. (eg
+%                     in Fieldtrip, it corresponds to the lay.cfg.outline
+%                     field)  (default [])
+% .clim             - set to fix the color limits
+%                     [cmin cmax] sets the color limits manually
+%                     'minmax' uses the minimum and maximum values in the
+%                     plot
+%                     'sym' makes the color values symmetric about a
+%                     reference point (e.g. [-0.7, 0.7] would be symmetric
+%                     about 0. cfg.climzero should be set if 'sym' is selected
+%                     (default [])
+% .climzero         - the 'zero point' in the data. For classification
+%                     accuracy it should correspond to chance level
+%                     (default 0.5)
+% .globalclim       - if 1 equalises the cfg.clim across all plots (if P>1)
+%                     (default 1)
+% .colorbar         - if 1, plots a colorbar. If globalclim=1, only one
+%                     colorbar is plotted, otherwise there is a separate
+%                     colorbar for each topography plotted
+% .cbtitle          - title for colorbar
+% .title            - title. can be cell array
+% .mark_chans       - [C x 1] vector with 0's and 1's, where a 1 specifes
+%                     that the according channel should be marked.
+%                     Alternatively, if multiple topographies are given, 
+%                     a matrix [C x M] can be provided
+% .res              - resolution of the topography grid in pixel x pixel.
+%                     Default 100 gives a 100 x 100 pixel grid
+% .nrow, .ncol      - if multiple topographies are given, nrow and ncol
+%                     specify the number of rows and columns in the subplot
+%
+% Returns:
+% h        - struct with handles to the graphical elements 
+
+% (c) Matthias Treder 2017
+
+if isvector(topo)
+    topo = topo(:); % make sure topo is a column vector
+end
+
+[~, P] = size(topo);
+
+mv_set_default(cfg,'outline',{});
+mv_set_default(cfg,'clim',[]);
+mv_set_default(cfg,'climzero', 0.5);
+mv_set_default(cfg,'globalclim',0);
+mv_set_default(cfg,'colorbar',1);
+mv_set_default(cfg,'cbtitle','');
+mv_set_default(cfg,'title','');
+mv_set_default(cfg,'mark_chans',[]);
+mv_set_default(cfg,'res', 100);
+mv_set_default(cfg,'ncol',ceil(sqrt(P)));
+mv_set_default(cfg,'nrow',ceil(P/cfg.ncol));
+
+if ~iscell(cfg.outline), cfg.outline = {cfg.outline}; end
+if ~iscell(cfg.title), cfg.title = {cfg.title}; end
+
+if ~isempty(cfg.mark_chans)
+    if isvector(cfg.mark_chans)
+        cfg.mark_chans = repmeat(cfg.mark_chans(:),[1 P]);
+    end
+end
+
+h= struct();
+
+%% Data extrapolation
+
+% If an cfg.outline is provided, data can be extrapolated beyond the sensor 
+% positions to 'fill up' the scalp cfg.outline. 
+% Since griddata can only interpolate (not extrapolate), we add the scalp
+% points as extra points with values equal to cfg.climzero.
+
+% Specify x and y limits of the grid
+if ~isempty(cfg.outline)
+    % assume that the first cell of cfg.outline specifies the scalp cfg.outline
+    xminmax = [min(cfg.outline{1}(:,1)), max(cfg.outline{1}(:,1))];
+    yminmax = [min(cfg.outline{1}(:,2)), max(cfg.outline{1}(:,2))];
+else
+    % if no cfg.outline is provided, we just interpolate within the space
+    % spanned by the channel positions
+    xminmax = [min(pos(:,1)), max(pos(:,1))];
+    yminmax = [min(pos(:,2)), max(pos(:,2))];
+end
+
+% Create grid
+linx = linspace(xminmax(1), xminmax(2), cfg.res);
+liny = linspace(yminmax(1), yminmax(2), cfg.res);
+[xq,yq] = meshgrid(linx, liny);
+
+% For extrapolation within the cfg.outline, a mask needs to be created that
+% masks out everything outside the scalp cfg.outline
+if ~isempty(cfg.outline)
+    mask = double(inpolygon(xq,yq, cfg.outline{1}(:,1), cfg.outline{1}(:,2)));
+    mask(mask==0) = nan;
+end
+
+%% Plot
+clf
+for mm=1:P
+    h.ax(mm) = subplot(cfg.nrow,cfg.ncol,mm);
+    cla
+    
+    % Interpolate data 
+    if ~isempty(cfg.outline)
+        % Use the v4 option which gives us extrapolation as well
+        topo_grid = griddata(pos(:,1), pos(:,2), topo(:,mm), xq, yq, 'v4');
+        % Mask out the parts outside of the cfg.outline
+        topo_grid = topo_grid  .* mask;
+    else
+        topo_grid = griddata(pos(:,1), pos(:,2), topo(:,mm), xq, yq);
+    end
+    
+    % Plot topography
+    contourf(xq, yq, topo_grid,50, 'LineStyle','none');
+    hold on
+    contour(xq, yq, topo_grid,'LineColor','k','LineStyle','-','LevelList',linspace(min(topo(:,mm)),max(topo(:,mm)),5));
+    
+    % Color limits
+    if ischar(cfg.clim)
+        if strcmp(cfg.clim,'sym')
+            cl = get(gca,'CLim');
+            set(gca,'CLim',[-1,1] * max(abs(cl-cfg.climzero) ) + cfg.climzero )
+        end
+    elseif ~isempty(cfg.clim)
+        set(gca,'cfg.clim',cfg.clim);
+    end
+    
+    % Plot channels
+    plot(pos(:,1), pos(:,2), 'k.','MarkerSize',0.5)
+    
+    % Mark channels
+    if ~isempty(cfg.mark_chans)
+        plot(pos(cfg.mark_chans(:,mm)==1,1), pos(cfg.mark_chans(:,mm)==1,2), 'k+','MarkerSize',10)
+    end
+    
+    % Plot cfg.outline if given
+    if ~isempty(cfg.outline)
+        hold on
+        for cc=1:numel(cfg.outline)
+            lineopt = {'LineWidth', 1, 'Color', 'k'};
+            hold on
+            plot(cfg.outline{cc}(:,1),cfg.outline{cc}(:,2),lineopt{:})
+            % adapt xlim and ylim if necessary
+            xl = xlim;
+            yl = ylim;
+            xlim([min(xl(1),min(cfg.outline{cc}(:,1))), max(xl(2),max(cfg.outline{cc}(:,1)))]);
+            ylim([min(yl(1),min(cfg.outline{cc}(:,2))), max(yl(2),max(cfg.outline{cc}(:,2)))]);
+        end
+    end
+    
+    % colorbar
+    if ~cfg.globalclim && cfg.colorbar 
+        h.colorbar(mm) = colorbar('Location','SouthOutside');
+        set(get(h.colorbar(mm),'title'),'string',cfg.cbtitle);
+    end
+    
+    % title
+    title(cfg.title{max(mod(mm,numel(cfg.title)),1)},'Interpreter','none')
+    
+    axis off
+end
+
+
+if cfg.globalclim
+    cl = get(h.ax,'CLim');
+    cl = cat(1,cl{:});
+    cl = [min(cl(:,1)), max(cl(:,2))];
+    
+    if strcmp(cfg.clim,'sym')
+        cl = get(gca,'CLim');
+        set(gca,'CLim',[-1,1] * max(abs(cl-cfg.climzero) ) + cfg.climzero )
+    else
+        set(h.ax,'CLim', cl);
+    end
+    
+    if cfg.colorbar
+        if ceil(sqrt(P)) ~= floor(sqrt(P)) 
+            % if the subplot grid is not completely filled up, we use the
+            % next subplot to produce a colorbar
+            h.ax(P+1) = subplot(cfg.nrow,cfg.ncol,P+1);
+            h.colorbar = colorbar('Location','WestOutside');
+        else
+            h.colorbar = colorbar('Location','EastOutside');
+        end
+        caxis(cl)
+        axis off
+    end
+
+end
