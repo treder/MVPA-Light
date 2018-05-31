21e9345034a3a945db96c0cf37ad89382063b0de
Matthias Treder <matthias.treder@gmail.com>
2018-06-01 00:03:16 +0100

    cleanup

--- classifier/test_lda.m
+++ classifier/test_lda.m
@@ -3,7 +3,7 @@ function [clabel,dval] = test_lda(cf,X)
 % decision values, and posterior probabilities.
 % 
 % Usage:
-% [clabel,dval,post] = test_lda(cf,X)
+% [clabel,dval] = test_lda(cf,X)
 % 
 %Parameters:
 % cf             - struct describing the classifier obtained from training 
@@ -15,7 +15,7 @@ function [clabel,dval] = test_lda(cf,X)
 %Output:
 % clabel        - predicted class labels (1's and 2's)
 % dval          - decision values, i.e. distances to the hyperplane or
-%                 class probabilities
+%                 probabilities
 
 dval = X*cf.w + cf.b;
 clabel= double(dval >= 0) + 2*double(dval < 0);
@@ -24,19 +24,8 @@ if cf.prob==1
     % To obtain posterior probabilities, we evaluate a multivariate normal
     % pdf at the test data point. As decision values, we output relative
     % class probabilities for class 1
-    prob1= mvnpdf(X, cf.mu1', cf.C);
-    prob2= mvnpdf(X, cf.mu2', cf.C);
+    prob1= mvnpdf(X, cf.mu1', cf.Sw/cf.N);
+    prob2= mvnpdf(X, cf.mu2', cf.Sw/cf.N);
     dval= prob1*cf.prior1 ./ (prob1*cf.prior1 + prob2*cf.prior2);
-
-    % The following code does not work, since normpdf on the projections
-    % does not yield results that are equivalent to mvnpdf on the
-    % non-projections
-    % For the posterior probability, we do not need to evaluate the
-    % multivariate normal pdf. Instead, we can simply evaluate the 1D
-    % normal pdf after projecting the class means and C onto w - this is
-    % faster
-%     prob1= normpdf(X * cf.w, cf.m1, cf.sigma);
-%     prob2= normpdf(X * cf.w, cf.m2, cf.sigma);
-%     dval= prob1*cf.prior1 ./ (prob1*cf.prior1 + prob2*cf.prior2);
 
 end
