8fda8f0c66a31f5ee34db68d233a34b54884fe83
Matthias Treder <matthias.treder@gmail.com>
2018-06-19 13:32:31 +0100

    add

--- unittests/unittest_classifier_kernel_fda.m
+++ unittests/unittest_classifier_kernel_fda.m
@@ -2,42 +2,67 @@
 %
 % Classifier: kernel_fda
 
-rng(42)
+rng(42)   %% do not change - might affect 
 tol = 10e-10;
 mf = mfilename;
 
-% Random data
-X = randn(1000,100);
-clabel = randi(2, size(X,1),1);
-
-%% check "scale" parameter: if scale = 1, training data should be scaled such that mean(class1)=1 and mean(class2)=-1
+%% providing kernel matrix directly VS calculating it from scratch should give same result
+gamma = 10e1;
 
 % Get classifier params
 param = mv_get_classifier_param('kernel_fda');
-param.scale = 1;
+param.gamma  = gamma;
+param.kernel = 'rbf';
 
-% Train and test classifier
-cf = train_lda(param, X, clabel);
-[~,dval] = test_lda(cf,X);
+% 1 -provide kernel matrix directly
+K = rbf_kernel(struct('gamma',gamma),X);
+param.kernel_matrix = K;
+cf_kernel = train_kernel_fda(param, X, clabel);
 
-% Does class 1 project to +1 ?
-unittest_print_result('check scale parameter for class 1',1, mean(dval(clabel==1)), tol);
+% 2 - do not provide kernel matrix (it is calculated in train_kernel_fda)
+param.kernel_matrix = [];
+cf_nokernel = train_kernel_fda(param, X, clabel);
 
-% Does class 2 project to -1 ?
-unittest_print_result('check scale parameter for class 2',-1, mean(dval(clabel==2)), tol);
-
-%% check "prob" parameter: if prob = 1, probabilities should be returned 
-
-% Get classifier params
-param = mv_get_classifier_param('lda');
-param.prob = 1;
-
-% Train and test classifier
-cf = train_lda(param, X, clabel);
-[~,dval] = test_lda(cf,X);
+% Compare solutions - the discriminant
+% axes can be in different order, so we look whether there's (nclasses-1)
+% 1's in the cross-correlation matrix
+C = abs(cf_kernel.A' * cf_nokernel.A); % cross-correlation since all axes have norm = 1
+C = sort(C(:),'descend'); % find the largest correlation values
+d = all(C(1:nclasses-1) - 1 < 10e-4);
 
 % Are all returned values between 0 and 1?
-unittest_print_result('check prob parameter',1, all(abs(dval)<=1),  tol);
+unittest_print_result('providing kernel matrix vs calculating it from scratch should be equal',1, d, tol);
+
+%% check classifier on multi-class spiral data: linear classifier should near chance, RBF kernel should be near 100%
+
+% Create spiral data
+N = 1000;
+nrevolutions = 2;       % how often each class spins around the zero point
+nclasses = 4;
+prop = 'equal';
+scale = 0;
+[X,clabel] = simulate_spiral_data(N, nrevolutions, nclasses, prop, scale, 1);
+
+%%% LINEAR kernel: cross-validation
+cfg             = [];
+cfg.classifier  = 'kernel_fda';
+cfg.feedback    = 0;
+
+acc_linear = mv_crossvalidate(cfg,X,clabel);
+
+%%% RBF kernel: cross-validation
+cfg.param.kernel    = 'rbf';
+cfg.param.gamma     = 10e1;
+acc_rbf = mv_crossvalidate(cfg,X,clabel);
+
+% Since CV is a bit chance-dependent: tolerance of 2%
+tol = 0.02;
+
+% For linear kernel: close to chance?
+unittest_print_result('classif spiral data (linear kernel)',1/nclasses, acc_linear, tol);
+
+% For RBF kernel: close to 1
+unittest_print_result('classif spiral data (RBF kernel)',1, acc_rbf, tol);
 
 %% check "lambda" parameter: if lambda = 1, w should be collinear with the difference between the class means
 
@@ -60,7 +85,7 @@ unittest_print_result('check w parameter for lambda=1 (equal to diff of class me
 %% Cross-validation: performance for well-separated classes should be 100%
 nsamples = 100;
 nfeatures = 10;
-nclasses = 2;
+nclasses = 5;
 prop = [];
 scale = 0.0001;
 do_plot = 0;
@@ -77,10 +102,9 @@ expect = 1;
 cfg = [];
 cfg.feedback        = 0;
 cfg.metric          = 'acc';
-cfg.classifier      = 'lda';
+cfg.classifier      = 'kernel_fda';
 cfg.param           = [];
-cfg.param.lambda    = 'auto';
-
+cfg.param.kernel    = 'linear';
 
 actual = mv_crossvalidate(cfg, X, clabel);
 
