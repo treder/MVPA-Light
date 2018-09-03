4a8ff85336adc5c6af1c89389b8b623eec2abf49
Matthias Treder <matthias.treder@gmail.com>
2018-09-03 13:13:34 +0100

    add prob

--- examples/example1_train_and_test.m
+++ examples/example1_train_and_test.m
@@ -36,13 +36,19 @@ X = squeeze(mean(dat.trial(:,:,ival_idx),3));
 % Get default hyperparameters for the LDA classifier
 param = mv_get_classifier_param('lda');
 
+% We also want to calculate class probabilities (prob variable) for each 
+% sample (do not use if not explicitly required since it slows down 
+% calculations a bit)
+param.prob  = 1;
+
 % Train an LDA classifier
 cf = train_lda(param, X, clabel);
 
 % Test classifier on the same data: the function gives the predicted
-% labels (predlabel) and the decision values (dval) which represent the
-% distance to the hyperplane
-[predlabel, dval] = test_lda(cf, X);
+% labels (predlabel), the decision values (dval) which represent the
+% distance to the hyperplane, and the class probability for the sample
+% belonging to class 1 (prob)
+[predlabel, dval, prob] = test_lda(cf, X);
 
 % To calculate classification accuracy, compare the predicted labels to
 % the true labels and take the mean
@@ -61,3 +67,16 @@ hold on
 plot(xlim, [0 0],'k--')
 ylabel('Decision values')
 xlabel('Class')
+title('Distribution of decision values')
+
+% Look at the distribution of the probabilities. prob should be higher
+% for clabel 1 (attended deviant) than clabel 2 (unattended
+% deviant)
+figure(3)
+clf
+boxplot(prob, clabel)
+hold on
+plot(xlim, [0.5 0.5],'k--')
+ylabel('Probability for class 1')
+xlabel('Class')
+title('Distribution of class probabilities')
