5a3bbc57567832c08140f533ab9349860317db4a
Matthias Treder <matthias.treder@gmail.com>
2018-12-03 16:48:16 +0000

    @

--- test/testcode_logreg.m
+++ test/testcode_logreg.m
@@ -18,10 +18,17 @@ tic
 cf = train_lda(param, X, clabel);
 toc
 
+%% PCA on data
+X = detrend(X);
+
+[U,E] = eig(cov(X));
+
+Xp = X*U;
+
 %% -- Logistic regression
 param = mv_get_classifier_param('logreg');
-param.lambda = logspace(-6,2,30); % 2
-param.plot = 1;
+param.lambda = 1; %logspace(-6,2,30); % 2
+% param.plot = 1;
 param.tolerance = 1e-6;
 param.polyorder = 2;
 
@@ -30,11 +37,19 @@ param.predict_regularisation_path = 0;
 tic
 rng(1)
 % profile on
-cf = train_logreg(param, zscore(X), clabel);
+% cf = train_logreg(param, zscore(X), clabel);
+% cf = train_logreg(param, zscore(Xp), clabel);
+
+%%% Compare logreg on original X and on PCs - should be identical
+
+cf = train_logreg(param, X, clabel);
+cf2 = train_logreg(param, Xp, clabel);
 % cf = train_logreg(param, X(:,[1,21]), clabel);
 toc
 % profile viewer
 % profile off
+
+[cf.w , U*cf2.w]
 
 %% Copy back into train_logreg and run to compare the optimisation to the 
 %% standard Matlab solver FSOLVE
