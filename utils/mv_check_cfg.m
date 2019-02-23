d24d09f0b4bc962c844f0dcbab4d4c3a2ba30260
Matthias Treder <matthias.treder@gmail.com>
2019-02-23 19:02:43 +0000

    check classifier and metrics

--- utils/mv_check_cfg.m
+++ utils/mv_check_cfg.m
@@ -1,6 +1,7 @@
 function mv_check_cfg(cfg)
-% Performs some sanity checks on cfg and checks whether all parameters
-% are written in lowercase
+% Performs some sanity checks on cfg 
+
+%% Check whether all parameters are written in lowercase
 
 fn = fieldnames(cfg);
 
@@ -21,3 +22,26 @@ if isfield(cfg,'param') && isstruct(cfg.param)
     end
 end
 
+%% check whether different metrics are compatible with each other 
+% eg 'confusion' does not work with 'tval' because the former requires
+% class labels as ouput whereas the latter requires dvals
+incompatible_metrics = { 'confusion' {'auc' 'tval' 'dval'};
+    };
+
+idx = find(ismember(incompatible_metrics(:,1), cfg.metric));
+
+if any(idx)
+    error('The metric %s cannot be used together with %s', incompatible_metrics{idx(1),1}, strjoin(incompatible_metrics{idx(1),2}))
+end
+
+%% check whether classifier and metric are compatible (eg 'auc' does not work for multiclass_lda)
+
+% Combinations of classifier and metrics that do not work together
+classifier_metric = { 'multiclass_lda' {'auc' 'tval' 'dval'};
+                      'kernel_fda'     {'auc' 'tval' 'dval'};
+    };
+
+idx = find(ismember(classifier_metric(:,1), cfg.classifier));
+if any(idx) && any(ismember(classifier_metric{idx,2}, cfg.metric))
+    error('The following metrics cannot be used with %s: %s', cfg.classifier, strjoin(classifier_metric{idx,2}))
+end
