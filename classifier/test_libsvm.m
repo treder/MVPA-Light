3f4a7d418e376b6c4a58cc36adb255e65cf2ea42
Matthias Treder <matthias.treder@gmail.com>
2019-05-16 12:03:05 +0200

    allow for precompted kernel

--- classifier/test_libsvm.m
+++ classifier/test_libsvm.m
@@ -12,7 +12,15 @@ function [clabel,dval] = test_libsvm(cf,X)
 % clabel        - predicted class labels
 % dval          - decision values
 
-[clabel, ~, dval] = svmpredict(zeros(size(X,1),1), X, cf.model,'-q');
+if cf.kernel_type == 4
+    % kernel has been precomputed - we only pass on the kernel matrix, not
+    % the data
+    nK = size(cf.kernel_matrix,1);
+    [clabel, ~, dval] = svmpredict(zeros(nK,1), [(1:nK)', cf.kernel_matrix], cf.model,'-q');
+else
+    % kernel is compute in svmtrain, pass on data
+    [clabel, ~, dval] = svmpredict(zeros(size(X,1),1), X, cf.model,'-q');
+end
 
 % Note that dvals might be sign-reversed in some cases,
 % see http://www.csie.ntu.edu.tw/~cjlin/libsvm/faq.html#f430
