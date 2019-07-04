ac61f7bbe4a7a0edd15e68f163c146d1628a889f
Matthias Treder <matthias.treder@gmail.com>
2019-07-04 16:27:18 +0100

    add kernel

--- examples/example_svm.m
+++ examples/example_svm.m
@@ -10,13 +10,21 @@ ival_idx = find(dat.time >= 0.6 & dat.time <= 0.8);
 X = squeeze(mean(dat.trial(:,:,ival_idx),3));
 
 X = zscore(X);
-%% SVM
+
+%% Precompute kernel matrix
+cfg = [] ;
+cfg.kernel = 'rbf';
+cfg.gamma = 1;
+cfg.regularize_kernel = 0;
+
+K = compute_kernel_matrix(cfg, dat.trial);
+
+%% train SVM
 close all
 
 param = mv_get_classifier_param('svm');
-param.C = 10;
+param.c = 10;
 param.tolerance = 10e-10;
-
 
 tic
 rng(1)
@@ -25,6 +33,14 @@ cf = train_svm(param, X, clabel);
 % profile viewer
 % profile off
 toc
+
+%% train SVM using kernel matrix
+param = mv_get_classifier_param('svm');
+param.kernel = 'precomputed';
+
+cf = train_svm(param, squeeze(K(:,:,30)), clabel);
+
+
 %%
 [predlabel, dval] = test_svm(cf, X);
 
