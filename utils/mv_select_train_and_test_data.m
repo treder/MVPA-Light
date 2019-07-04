16713d845c31c8480bc71d94602588c2a3dc9125
Matthias Treder <matthias.treder@gmail.com>
2019-07-04 16:27:28 +0100

    add kernel

--- utils/mv_select_train_and_test_data.m
+++ utils/mv_select_train_and_test_data.m
@@ -1,4 +1,4 @@
-function [Xtrain, trainlabel, Xtest, testlabel] = mv_select_train_and_test_data(cfg, X, clabel, train_indices, test_indices)
+function [Xtrain, trainlabel, Xtest, testlabel] = mv_select_train_and_test_data(X, clabel, train_indices, test_indices, is_kernel_matrix)
 %Splits the data into training and test set. 
 %
 %Usage:
@@ -6,42 +6,44 @@ function [Xtrain, trainlabel, Xtest, testlabel] = mv_select_train_and_test_data(
 %    mv_select_train_and_test_data(cfg, X, clabel, train_indices, test_indices)
 %
 %Parameters:
-% X              - [... x ... x ... x] data matrix
+% X              - [samples x features x ... ] data matrix -OR-
+%                  [samples x samples  x ... ] kernel matrix
 % clabel         - [samples x 1] vector of class labels
 % train_indices  - vector of indices of train samples
 % test_indices   - vector of indices of test samples
-%
-% cfg          - struct with parameters:
-% .sample_dimension - vector specifying which dimension of the array X
-%                     contains the samples (can be multiple). Samples are
-%                     selected along this dimension(s).
-%
+% is_kernel_matrix - if 1, X represents a kernel matrix
 %
 %Returns:
 % Xtrain, trainlabel - train data and class labels
 % Xtest, testlabel   - test data and class labels
 
-if all(cfg.sample_dimension == 1)
+if ~is_kernel_matrix
     % Standard case: samples are in the first dimension only
     
     % Get train and test data
-    Xtrain = X(train_indices,:,:,:,:,:,:,:,:,:,:,:,:);
-    Xtest= X(test_indices,:,:,:,:,:,:,:,:,:,:,:,:);
+    Xtrain = X(train_indices,:,:,:,:,:,:,:,:,:,:,:,:,:,:);
+    Xtest= X(test_indices,:,:,:,:,:,:,:,:,:,:,:,:,:,:);
     
 else
-    % Non-standard case: sample dimension is not 1, or there is multiple
-    % sample dimensions.
-    % In this case, we will build a colon operator s to extract
-    % the train/test samples
-    s_train = repmat({':'},[1, ndims(X)]);
-    s_test  = repmat({':'},[1, ndims(X)]);
+    % kernel matrix is provided: we need a [train samples x train samples]
+    % kernel matrix for training and a [train samples x test samples]
+    % matrix for testing
+    Xtrain = X(train_indices,train_indices,:,:,:,:,:,:,:,:,:,:,:,:,:);
+    Xtest= X(train_indices,test_indices,:,:,:,:,:,:,:,:,:,:,:,:,:,:);
     
-    s_train(cfg.sample_dimension) = {train_indices};
-    s_test(cfg.sample_dimension) = {test_indices};
-    
-    % Get train and test data
-    Xtrain = X(s_train{:});
-    Xtest= X(s_test{:});
+%     % Non-standard case: sample dimension is not 1, or there is multiple
+%     % sample dimensions.
+%     % In this case, we will build a colon operator s to extract
+%     % the train/test samples
+%     s_train = repmat({':'},[1, ndims(X)]);
+%     s_test  = repmat({':'},[1, ndims(X)]);
+%     
+%     s_train(cfg.sample_dimension) = {train_indices};
+%     s_test(cfg.sample_dimension) = {test_indices};
+%     
+%     % Get train and test data
+%     Xtrain = X(s_train{:});
+%     Xtest= X(s_test{:});
 end
 
 % Get train and test class labels
