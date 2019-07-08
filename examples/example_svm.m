b7c4fc31c0c8d70ab7b93f612e4917711ebdaaa9
Matthias Treder <matthias.treder@gmail.com>
2019-07-08 17:06:27 +0100

    add precomputed kernel

--- examples/example_svm.m
+++ examples/example_svm.m
@@ -5,26 +5,21 @@ clear all
 % Load data (in /examples folder)
 [dat,clabel] = load_example_data('epoched2');
 
+pparam = mv_get_preprocess_param('zscore');
+[~, dat.trial] = mv_preprocess_zscore(pparam, dat.trial);
+
 % Average activity in 0.6-0.8 interval (see example1)
 ival_idx = find(dat.time >= 0.6 & dat.time <= 0.8);
 X = squeeze(mean(dat.trial(:,:,ival_idx),3));
 
-X = zscore(X);
-
-%% Precompute kernel matrix
-cfg = [] ;
-cfg.kernel = 'rbf';
-cfg.gamma = 1;
-cfg.regularize_kernel = 0;
-
-K = compute_kernel_matrix(cfg, dat.trial);
 
 %% train SVM
 close all
 
 param = mv_get_classifier_param('svm');
-param.c = 10;
+% param.c = 10;
 param.tolerance = 10e-10;
+param.plot = 1;
 
 tic
 rng(1)
@@ -34,15 +29,88 @@ cf = train_svm(param, X, clabel);
 % profile off
 toc
 
-%% train SVM using kernel matrix
+
+%% optimise hyperparameter
+param = mv_get_classifier_param('svm');
+param.kernel = 'polynomial';
+% param.kernel = 'rbf';
+param.gamma  = [0.001, 0.01, 0.033, 0.1, 100, 1000];
+% param.gamma  = 0.00333;
+param.degree = [1, 2, 3];
+param.c = 'auto';
+
+cf = train_svm(param, X, clabel);
+
+%% Precompute kernel matrix
+cfg = [] ;
+cfg.kernel = 'rbf';
+cfg.gamma = 0.0333;
+cfg.regularize_kernel = 10^-10;
+
+K = compute_kernel_matrix(cfg, dat.trial);
+
+%% create averaged kernel matrix
+pparam = mv_get_preprocess_param('average_samples');
+pparam.is_kernel_matrix = 1;
+
+[~, K_average, clabel_average] = mv_preprocess_average_samples(pparam, K, clabel);
+
+%% classify across time 
+cfg = [] ;
+cfg.classifier  = 'svm';
+cfg.metric      = 'auc';
+cfg.param       = [];
+cfg.param.c     = 1;
+
+cfg.param.kernel = 'rbf';
+
+[perf, res] = mv_classify_across_time(cfg, dat.trial, clabel);
+
+mv_plot_result(res, dat.time)
+
+%% train SVM using precomputed kernel matrix
 param = mv_get_classifier_param('svm');
 param.kernel = 'precomputed';
 
 cf = train_svm(param, squeeze(K(:,:,30)), clabel);
 
+%% classify across time using precomputed kernel matrix
+cfg = [] ;
+cfg.classifier  = 'svm';
+cfg.metric      = 'auc';
+cfg.param       = [];
+cfg.param.c     = 1;
 
-%%
-[predlabel, dval] = test_svm(cf, X);
+cfg.param.kernel = 'precomputed';
 
-% Calculate AUC
-auc = mv_calculate_performance('auc', dval, clabel);
+[perf, res] = mv_classify_across_time(cfg, K, clabel);
+
+mv_plot_result(res, dat.time)
+
+%% classify across time using precomputed averaged kernel matrix
+cfg = [] ;
+cfg.classifier  = 'svm';
+cfg.metric      = 'auc';
+cfg.param       = [];
+cfg.param.c     = 1;
+cfg.param.kernel = 'precomputed';
+
+[perf, res_av] = mv_classify_across_time(cfg, K_average, clabel_average);
+
+mv_plot_result({res, res_av}, dat.time)
+
+%% classify across time using precomputed kernel matrix with nested kernel averaging
+cfg = [] ;
+cfg.classifier  = 'svm';
+cfg.metric      = 'auc';
+cfg.param       = [];
+cfg.param.c     = 1;
+cfg.param.kernel = 'precomputed';
+
+cfg.preprocess          = 'average_samples';
+cfg.preprocess_param    = [];
+cfg.preprocess_param.is_kernel_matrix = 1;
+
+[perf, res_av2] = mv_classify_across_time(cfg, K, clabel);
+
+mv_plot_result({res, res_av, res_av2}, dat.time)
