742cc48c05b196348ab71ecf6abe66e4b6e0762f
Matthias Treder <matthias.treder@gmail.com>
2019-08-12 20:22:22 +0100

    finished

--- examples/example6_classify_multidimensional_data.m
+++ examples/example6_classify_multidimensional_data.m
@@ -33,7 +33,7 @@ dat3.trial = zscore(dat3.trial);
 
 %% compare to mv_crossvalidate (based on example2_crossvalidate)
 % Take average in a time window so that X is [samples x features]
-ival_idx = find(dat.time >= 0.6 & dat.time <= 0.8);
+ival_idx = (dat.time >= 0.6 & dat.time <= 0.8);
 X = squeeze(mean(dat.trial(:,:,ival_idx),3));
 
 % Let's start doing cross-validation with mv_crossvalidate
@@ -60,7 +60,6 @@ perf2 = mv_classify(cfg, X, clabel);
 
 fprintf('mv_crossvalidate result: %2.5f\n', perf)
 fprintf('mv_classify result: %2.5f\n', perf2)
-
 
 %% compare to mv_classify_across_time 
 cfg =  [];
@@ -163,6 +162,7 @@ figure
 bar([auc, auc2])
 legend({'auc', 'auc2'})
 xlabel('Channel'), ylabel('AUC')
+grid on
 
 fprintf('Difference between auc and auc2: %2.2f\n', norm(auc-auc2)) % should be 0
 
@@ -187,13 +187,15 @@ cfg.dimension_names = {'samples','channels','time points'};
 % Let's first just classify without neighbours
 perf = mv_classify(cfg, dat.trial, clabel);
 
-% The third dimension (third) is used as search dimension. Now, for
-% every time point, we also want to take up the immediately preceding and
-% immediately following time points as features. To this end, we define a
+% The third dimension (time) serves as search dimension. Now, for
+% every time point, we also want to take up the 2 immediately preceding and
+% 2 immediately following time points as features. To this end, we define a
 % [time x time] neighbourhood matrix that has ones on the diagonals and
 % subdiagonals. This defines the extent of the searchlight.
 O = ones(size(dat.trial,3));
-O = O - triu(O,4) - tril(O,-4);
+O = O - triu(O,3) - tril(O,-3);
+
+O(1:10,1:20)
 
 cfg.neighbours = O;
 
@@ -206,21 +208,25 @@ hold all
 plot(perf2)
 legend({'without searchlight' ,'with searchlight'})
 
-%% Time-frequency classification
+%% --- Time-frequency classification [requires signal processing toolbox] ---
 % For this example, we will first calculate the time-frequency
 % representation using the spectrogram function. Note that this requires
 % the signal processing toolbox.
 
+%% Calculate spectrogram
 sz = size(dat.trial);
 
 % Set parameters for spectrogram
-win= kaiser(32, 18);
+win= chebwin(20);
 Fs = 1/mean(diff(dat.time));
-noverlap = 30;
+noverlap = 18;
 nfft = 64;
 
 % get number of frequencies and time points
-[S,F,T] = spectrogram(squeeze(dat.trial(nn,cc,:)), win, noverlap, nfft, Fs);
+[S,F,T] = spectrogram(squeeze(dat.trial(1,1,:)), win, noverlap, nfft, Fs);
+
+% Correct time T
+T = T + dat.time(1);
 
 freq = dat; 
 freq.trial = zeros([sz(1:2), numel(F), numel(T)]);
@@ -235,221 +241,134 @@ for nn=1:sz(1)
     end
 end
 
-
 %% Baseline correction
 pre = find(freq.time < 0);
-BL = mean(freq.trial(:,:,pre,:), 3);
+BL = mean(mean(freq.trial(:,:,:,pre), 4),1);
 
-%% Generalization across datasets
-% use subjects as an extra variable
+% calculate relative baseline 
+sz = size(freq.trial);
+BLmat = repmat(BL, [sz(1) 1 1 sz(4)]);
+freq.trial = (freq.trial - BLmat) ./ BLmat;
 
-%% --- 'Global' preprocessing ---
-% in 'global' preprocessing, the full dataset is preprocessed once before
-% the classification analysis is started. Here, we will average the
-% samples first.
-
-%% Average samples
-% see mv_preprocess_average_samples.m for details on sample averaging
-
-% First, we need to obtain the default preprocessing parameters
-% using the function mv_get_preprocess_param.
-pparam = mv_get_preprocess_param('average_samples');
-
-% Now we call the preprocessing function by hand. All preprocessing
-% functions take preprocess_param, X, and clabel as inputs, and they return
-% the same set of parameters.
-[pparam, dat.trial_av, clabel_av] = mv_preprocess_average_samples(pparam, dat.trial, clabel);
-
-% we can now take the averaged data and new class labels forward and
-% perform classification
-
+%% Classification for each time-frequency point separately
 cfg = [];
-cfg.classifier  = 'lda';
-perf = mv_classify_across_time(cfg, dat.trial_av, clabel_av);
-
-%% Z-scoring
-% A very useful global preprocessing operation is z-scoring, whereby the
-% data is scaled and mean-centered. It is recommended in general, but it is
-% important for classifiers such as Logistic Regression for numerical
-% reasons. Furthermore, z-scoring brings all features on the same footing,
-% irrespective of the units they were measured in originally.
-
-pparam = mv_get_preprocess_param('zscore');
-[~, dat.trial] = mv_preprocess_zscore(pparam, dat.trial);
-
-
-%% Undersampling
-pparam = mv_get_preprocess_param('undersample');
-fprintf('Number of samples in each class before undersampling: %d %d\n', arrayfun(@(c) sum(clabel==c), 1:max(clabel)))
-[~, dat.trial, clabel] = mv_preprocess_undersample(pparam, dat.trial, clabel);
-fprintf('Number of samples in each class after undersampling: %d %d\n', arrayfun(@(c) sum(clabel==c), 1:max(clabel)))
-
-%% --- Nested preprocessing ---
-% Nested preprocessing can be used in conjunction with the high-level
-% functions of MVPA-Light such as mv_classify_across_time. The
-% preprocessing is applied on train set and test set separately. In some
-% cases (such as z-score), parameters (such as mean and std) are
-% calculated on the training data and then applied to the test data.
-
-[dat,clabel] = load_example_data('epoched2');
-
-% If we want to oversample the data, this must be done in a nested fashion:
-% only the train data is oversampled, this prevents identical samples
-% ending up in both train and test data.
-% 
-% By setting cfg.preprocess = 'oversample', MVPA-Light will call the
-% preprocessing function mv_preprocess_oversample internally to oversample
-% the train data.
-
-cfg = [];
-cfg.metric          = 'f1';
-cfg.classifier      = 'logreg';
-cfg.preprocess      = 'oversample';
-
-[~, res] = mv_classify_timextime(cfg, dat.trial, clabel);
-
-mv_plot_result(res);
-
-
-%% Averaging samples/instances
-% Here we explore the average_samples preprocessing method. Samples from
-% the same class are split into multiple groups and then the dataset is
-% replaced by the group means. This reduces the data and at the same time
-% increases SNR. 
-% To investigate this 
-cfg = [];
-cfg.metric          = 'acc';
 cfg.classifier      = 'lda';
-cfg.preprocess      = 'average_samples';
-
-group_sizes = [1, 3, 8];
-res = cell(1, numel(group_sizes));
-
-for ii=1:3
-    cfg.preprocess_param = [];
-    cfg.preprocess_param.group_size = group_sizes(ii);
-    
-    [~, res{ii}] = mv_classify_across_time(cfg, dat2.trial, clabel2);
-end
-
-%% Plot the results
-% We make two observations: increasing group size leads to higher classification
-% performance. At the same time, the errorbars get larger. The latter is
-% because after averaging, the effective number of samples is decreasing,
-% leading to a larger variability. This can partly be mitigated by having
-% more repeats.
-mv_plot_result(res, dat.time)
-
-legend(strcat({'Group size: '}, arrayfun(@(c) {num2str(c)}, group_sizes)), ...
-    'location', 'southeast')
-
-%% Kernel averaging for non-linear classification problems
-% Kernel averaging is the generalization of sample averaging to non-linear
-% kernels. Instead of averaging in input space, the averages are formed in
-% Reproducing Kernel Hilbert Space (RKHS). This can be done efficiently
-% using the kernel trick. In order to use kernel averaging, the kernels
-% need to be precomputed and the kernel matrix provided as input.
-
-% Precompute a kernel matrix for every time point
-kparam = [];
-kparam.kernel = 'rbf';
-kparam.gamma  = 1/30;
-kparam.regularize_kernel = 10e-1;
-K = compute_kernel_matrix(kparam, dat2.trial);
-
-cfg = [];
 cfg.metric          = 'auc';
-cfg.classifier      = 'svm'; % 'kernel_fda'
-cfg.param           = [];
-cfg.param.kernel    = 'precomputed'; % indicate that the kernel matrix is precomputed
-cfg.preprocess      = 'average_kernel';
 
-group_sizes = [1, 5];
-res = cell(1, numel(group_sizes));
+% samples are in dimension 1 and features in
+% dimension 2. The last two dimensions  will be automatically devised as search
+% dimensions.
+cfg.sample_dimension = 1;
+cfg.feature_dimension  = 2;
 
-for ii=1:2
-    cfg.preprocess_param = [];
-    cfg.preprocess_param.group_size = group_sizes(ii);
-    
-    % kernel matrix K serves as input
-    [~, res{ii}] = mv_classify_across_time(cfg, K, clabel2);
-end
+% optional: provide the names of the dimensions for nice output
+cfg.dimension_names = {'samples','channels','frequencies', 'time points'};
 
-mv_plot_result(res, dat.time)
-legend(strcat({'Group size: '}, arrayfun(@(c) {num2str(c)}, group_sizes)), ...
-    'location', 'southeast')
+perf = mv_classify(cfg, freq.trial, clabel);
 
-%% Preprocessing pipelines: concatenating preprocessing steps
-% Multiple preprocessing steps can be concatenated by providing cell arrays
-% for .preprocess and .preproces_param
+% perf is now a 2-D [frequencies x times] matrix of classification results
+figure
+imagesc(T,F,perf)
+colorbar
+xlabel('Time'), ylabel('Frequency')
+title('AUC at each time-frequency point')
 
-% To illustrate this, let's add a nested z-scoring to the sample averaging.
-% MVPA-Light carries out the operations in order: First, z-scoring is
-% performed on the data. The resultant data is then averaged.
+%% Classification for each time-frequency point separately including neighbours
+% Let's repeat the previous analysis but, for each time-frequency point,
+% let's include neighbouring frequencies and time points
+% frequencies. To this end, we need to create neighbourhood matrices for
+% each of the two search dimensions:
 
+[nsamples, nchannels, nfrequencies, ntimes] = size(freq.trial);
+
+% 1) create binary neighbourhood matrix for frequencies (includes a given
+% frequency and the two immediately preceding and following frequencies)
+O = ones(nfrequencies);
+O = O - triu(O,2) - tril(O,-2);
+freq_neighbours = O;
+
+freq_neighbours(1:5,1:10)
+
+% 2) create neighbourhood matrix for times (includes a given
+% time point and the two immediately preceding and following time points)
+O = ones(ntimes);
+O = O - triu(O,2) - tril(O,-2);
+time_neighbours = O;
+
+% Store both matrices together in a cell array
+cfg.neighbours = {freq_neighbours, time_neighbours};
+
+perf = mv_classify(cfg, freq.trial, clabel);
+
+% perf is a 2-D [frequencies x times] matrix of classification results
+figure
+imagesc(T,F,perf)
+colorbar
+xlabel('Time'), ylabel('Frequency')
+title('AUC at each time-frequency point including neighbours')
+
+%% Classification for each time point [using channels x frequencies as features]
 cfg = [];
-cfg.metric          = 'acc';
 cfg.classifier      = 'lda';
-cfg.preprocess      = {'zscore' 'average_samples' 'demean'};
+cfg.metric          = 'auc';
+cfg.dimension_names = {'samples','channels','frequencies', 'time points'};
 
-[perf, res] = mv_classify_across_time(cfg, dat2.trial, clabel2);
+cfg.sample_dimension  = 1;
+% Here, we designate both channels and frequencies as features. Since
+% cfg.flatten = 1 per default, both dimension will be flattened into a
+% single feature vector of length channels x frequencies
+cfg.feature_dimension  = [2, 3];
 
-%% Preprocessing pipelines: adding optional parameters
-% building on the previous example, let's now change the group size 
-% for the average_samples operation. It is the second operation in the
-% preprocessing pipeline, hence we need to make sure that .preprocess_param
-% is a cell array and that we set the group size in the second cell:
+perf = mv_classify(cfg, freq.trial, clabel);
 
-cfg.preprocess_param = {};
-cfg.preprocess_param{2} = [];
-cfg.preprocess_param{2}.group_size = 2;
+figure
+plot(T, perf)
+xlabel('Time')
 
-[perf2, res2] = mv_classify_across_time(cfg, dat2.trial, clabel2);
-
-cfg.preprocess_param
-
-mv_plot_result({res, res2}, dat.time)
-legend(strcat({'Group size: '}, {'5' '2'}))
-
-%% Preprocessing pipelines with searchlight
-% The pipelines work the same way with all high-level functions. Here, we
-% try out a pipeline consisting of oversampling -> z-scoring -> averaging
-% with mv_searchlight. We do not specify any neighbours, so every feature
-% is classified separately. 
-
-% Load data (in /examples folder)
-[dat, clabel, chans] = load_example_data('epoched3');
-
-% We want to classify only on the 300-500 ms window
-time_idx = find(dat.time >= 0.3  &  dat.time <= 0.5);
+%% Time-frequency classification with time generalization
+% Starting from the previous example, we perform time x time classification
+% (time generalization). All the channels/frequencies will be used as features, so
+% the feature vector has length channels x frequencies. 
+% Samples are in dimension 1 and features in
+% dimension 2. The last two dimensions  will be automatically devised as search
+% dimensions. However, this time we also need to indicate that we want to 
+% the 3rd dimension for generalization (time x time).
 
 cfg = [];
-cfg.average     = 1;
-cfg.metric      = 'auc';
-cfg.preprocess  = {'oversample' 'zscore' 'average_samples'};
-cfg.preprocess_param = {};
-cfg.preprocess_param{3} = [];
+cfg.classifier      = 'lda';
+cfg.metric          = 'auc';
+cfg.dimension_names = {'samples','channels','frequencies', 'time points'};
 
-% run for group sizes 1 and 5
-cfg.preprocess_param{3}.group_size = 1;
-[auc1, result1] = mv_searchlight(cfg, dat.trial(:,:,time_idx), clabel);
+cfg.sample_dimension  = 1;
+cfg.feature_dimension  = [2, 3];
+cfg.generalization_dimension = 4;
 
-cfg.preprocess_param{3}.group_size = 5;
-[auc5, result5] = mv_searchlight(cfg, dat.trial(:,:,time_idx), clabel);
-
-mv_plot_result({result1 , result5})
-
-%% Plot classification performance as a topography
-cfg_plot = [];
-cfg_plot.outline = chans.outline;
-figure
-mv_plot_topography(cfg_plot, auc1, chans.pos);
-set(gca,'CLim',[0.4, 0.8])
-title('AUC for group size 1')
+perf = mv_classify(cfg, freq.trial, clabel);
 
 figure
-mv_plot_topography(cfg_plot, auc5, chans.pos);
-set(gca,'CLim',[0.4, 0.8])
-title('AUC for group size 5')
+imagesc(T, F, perf)
+mv_plot_2D(perf, 'x', T, 'y', T)
+title('Time generalization using channels-x-frequencies as features')
 
+%% Time-frequency classification with frequency generalization
+% same as previous example, but we swap frequencies and times: time points
+% will serve as features, whereas we will train and test on different
+% frequencies. The result is a frequency x frequency plot.
 
+cfg = [];
+cfg.classifier      = 'lda';
+cfg.metric          = 'auc';
+cfg.dimension_names = {'samples','channels','frequencies', 'time points'};
+
+cfg.sample_dimension  = 1;
+% Relative to the previous example, we need to swap elements in the feature 
+% dimension and generalization dimension.
+cfg.feature_dimension  = [2, 4];   
+cfg.generalization_dimension = 3;
+
+perf = mv_classify(cfg, freq.trial, clabel);
+
+figure
+mv_plot_2D(perf, 'x', F, 'y', F)
+xlabel('Test frequency [Hz]'), ylabel('Train frequency [Hz]')
+title('Frequency generalization using channels-x-times as features')
