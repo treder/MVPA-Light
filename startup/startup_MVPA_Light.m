66e8ed1ca649f76f1c328bf1e27459cf719687f6
Matthias Treder <matthias.treder@gmail.com>
2019-11-28 11:35:37 +0000

    add model folder, mv kernel/optimization

--- startup/startup_MVPA_Light.m
+++ startup/startup_MVPA_Light.m
@@ -4,11 +4,11 @@ function startup_MVPA_Light
 MVPA_path = fileparts(fileparts(mfilename('fullpath')));
 
 addpath(MVPA_path);
-addpath(fullfile(MVPA_path,'classifier'));
-addpath(fullfile(MVPA_path,'classifier','kernel'));
-addpath(fullfile(MVPA_path,'classifier','optimisation'));
 addpath(fullfile(MVPA_path,'examples'));
 addpath(fullfile(MVPA_path,'external'));
+addpath(fullfile(MVPA_path,'kernel'));
+addpath(fullfile(MVPA_path,'model'));
+addpath(fullfile(MVPA_path,'optimization'));
 addpath(fullfile(MVPA_path,'plot'));
 addpath(fullfile(MVPA_path,'preprocess'));
 addpath(fullfile(MVPA_path,'statistics'));
