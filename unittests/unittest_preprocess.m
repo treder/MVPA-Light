dad9c4e4d64a1e49a0724d937f8edf3ed009f7e3
Matthias Treder <matthias.treder@gmail.com>
2021-01-11 15:03:39 +0000

    cfg.preprocess -> cfg.preprocess_fun

--- unittests/unittest_preprocess.m
+++ unittests/unittest_preprocess.m
@@ -1,6 +1,5 @@
 % Preprocessing unit test
 %
-
 rng(42)
 tol = 10e-10;
 
@@ -10,7 +9,7 @@ X = randn(N,40);
 clabel = randi(2, N, 1);
 
 cfg = [];
-cfg.preprocess = {};
+cfg.preprocess_fun = {};
 cfg.preprocess_param = {};
 
 oversample_param = mv_get_preprocess_param('oversample');
@@ -20,7 +19,7 @@ demean_param = mv_get_preprocess_param('demean');
 average_param = mv_get_preprocess_param('average_samples');
 
 %% try all preprocessing routeins + .is_train_set should be 1 after calling mv_preprocess once
-cfg.preprocess = {@mv_preprocess_oversample @mv_preprocess_undersample @mv_preprocess_zscore @mv_preprocess_demean @mv_preprocess_average_samples};
+cfg.preprocess_fun = {@mv_preprocess_oversample @mv_preprocess_undersample @mv_preprocess_zscore @mv_preprocess_demean @mv_preprocess_average_samples};
 cfg.preprocess_param = {oversample_param, undersample_param, zscore_param, demean_param, average_param};
 
 [cfg, X2, clabel2] = mv_preprocess(cfg, X, clabel);
