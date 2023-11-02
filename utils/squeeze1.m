cb18e328db1004f0d73451dda44c780a65d0aab2
treder <matthias.treder@gmail.com>
2023-11-02 18:45:43 +0000

    add

---
+++ utils/squeeze1.m
@@ -0,0 +1,12 @@
+function X = squeeze1(X)
+% Like squeeze but always preserves the 1st dimension. This deals with
+% cases where e.g. size(X) = [1, 1, 20] and size(squeeze(X)) = [20, 1]
+% whereas size(squeeze(X)) = [1, 20] is desirable. The latter case is
+% relevant e.g. in leaveout cross-validation where the first dimension is
+% the sample dimension and the test set contains only 1 sample.
+sz = size(X);
+
+% remove singleton dimensions except for the first dimension
+sz = [sz(1) setdiff(sz(2:end), 1)];
+
+X = reshape(X, sz);
