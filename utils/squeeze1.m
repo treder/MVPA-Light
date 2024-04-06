2882e5d615bda2ebd2541b0240d08379e2e354ef
treder <matthias.treder@gmail.com>
2024-04-06 18:02:11 +0100

    deal with case of size(X,1)=1 but >=2 non-singleton dims

--- utils/squeeze1.m
+++ utils/squeeze1.m
@@ -1,17 +1,24 @@
 function X = squeeze1(X)
-% Like squeeze but always preserves the 1st dimension. This deals with
+% Like squeeze but prevents unwanted transposes. This deals with
 % cases where e.g. size(X) = [1, 1, 20] and size(squeeze(X)) = [20, 1]
-% whereas size(squeeze(X)) = [1, 20] is desirable. The latter case is
+% whereas size(squeeze(X)) = [1, 20] would be desirable. The latter case is
 % relevant e.g. in leaveout cross-validation where the first dimension is
 % the sample dimension and the test set contains only 1 sample.
 sz = size(X);
-sz_end = sz(2:end);
 
-% remove singleton dimensions except for the first dimension
-sz = [sz(1) sz_end(~ismember(sz_end,1))];
+if sum(sz>1) > 1
+    % there's more than 2 non-singleton dimensions: we can simply use
+    % squeeze in this case
+    X = squeeze(X);
+else
+    sz_end = sz(2:end);
 
-if length(sz) == 1
-    sz = [sz 1];
+    % remove singleton dimensions except for the first dimension
+    sz = [sz(1) sz_end(~ismember(sz_end,1))];
+
+    if length(sz) == 1
+        sz = [sz 1];
+    end
+
+    X = reshape(X, sz);
 end
-
-X = reshape(X, sz);
