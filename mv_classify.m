211adb21cb41cf3a700cac67add7007ef0c0eac7
Jan Mathijs Schoffelen <j.schoffelen@donders.ru.nl>
2026-07-02 13:55:38 +0200

    changes to accommodate for undersampling during preprocessing

--- mv_classify.m
+++ mv_classify.m
@@ -382,6 +382,11 @@ if ~strcmp(cfg.cv,'none') && ~has_second_dataset
             sz_Xtrain = size(Xtrain);
             sz_Xtest = size(Xtest);
             
+            % JM ADDITION: NFEAT MIGHT HAVE CHANGED DURING PREPROCESSING
+            nfeat = [sz_Xtrain ones(1, numel(cfg.dimension_names) - ndims(X))];
+            nfeat = nfeat(feature_dim);
+            if isempty(nfeat), nfeat = 1; end
+
             for ix = dim_loop                       % ---- search dimensions ----
                                 
                 % Training data for current search position
